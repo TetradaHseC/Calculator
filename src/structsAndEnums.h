a20425ecf5411269bd5179037ceb4f7403ed9771
yoloroy <slart2108@gmail.com>
2021-02-10 19:17:36 +0300

    :sparkles: add ParsedExpression structure it is necessary for the convenience of storage and transfer

--- src/structsAndEnums.h
+++ src/structsAndEnums.h
@@ -1,8 +1,18 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
-typedef struct { double real, fake; char *definedName; } ComplexNumber;
-
 typedef enum { EPlus, EMinus, EMultiply, EDivide, EOpenParenthesis, ECloseParenthesis, ETg, ELn, ECos, ESin, EPow, EAbs, EExp, EMag, ELog, EReal, EImag, ESqrt, EPhase } Operation;
+
+typedef struct {
+    double real, fake;
+    char *definedName;
+} ComplexNumber;
+
+typedef struct {
+    int numc;
+    ComplexNumber *numv;
+    int opc;
+    Operation *opv;
+} ParsedExpression;
 
 #endif//STRUCTS_H
