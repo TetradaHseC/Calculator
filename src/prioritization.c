a2127a45ee47039bf5a5ccfc6b6898059d51e427
Cyber Doc1or <40675707+xSVTx@users.noreply.github.com>
2021-02-16 19:31:50 +0300

    Added priority determiner

---
+++ src/prioritization.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int CheckThePriority(char operation[5]){
+    if(!strcmp(operation, "sqrt"))
+        operation[0] = ':';
+    switch(* operation){
+        case '+': case'-': return 0;
+        case '*': case'/': return 1;
+        case '^': case ':': return 2;
+        case '(': case ')': return 3;
+        default:  return 4;
+    }
+}
