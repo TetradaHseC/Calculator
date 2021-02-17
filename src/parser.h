4796edcc0f1fd1419259de7a9b39218151adc183
yoloroy <slart2108@gmail.com>
2021-02-17 13:09:17 +0300

    :art: add comments about freeing strings

--- src/parser.h
+++ src/parser.h
@@ -2,8 +2,10 @@
 #define PARSER_H
 #include "structsAndEnums.h"
 
+/// DictEntire.value.numv[n].definedName must be freed after usage if it not NULL
 DictEntire ParseDefinition(char *line);
 
+/// numv[n].definedName must be freed after usage if it not NULL
 void ParseExpression(char *line, int *numc, ComplexNumber **numv, int *opc, Operation **opv);
 
 #endif//PARSER_H
