786800046e341b2bead96bdbd2c58fa449739164
yoloroy <slart2108@gmail.com>
2021-02-17 19:45:42 +0300

    Update structsAndEnums.h

--- src/structsAndEnums.h
+++ src/structsAndEnums.h
@@ -1,7 +1,7 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
-typedef enum { EPlus, EMinus, EUnaryMinus, EMultiply, EDivide, EOpenParenthesis, ECloseParenthesis, ETg, ELn, ECos, ESin, EPow, EAbs, EExp, EMag, ELog, EReal, EImag, ESqrt, EPhase } Operation;
+typedef enum { EComma, EPlus, EMinus, EUnaryMinus, EMultiply, EDivide, EOpenParenthesis, ECloseParenthesis, ETg, ELn, ECos, ESin, EPow, EAbs, EExp, EMag, ELog, EReal, EImag, ESqrt, EPhase } Operation;
 
 typedef struct {
     double real, fake;
